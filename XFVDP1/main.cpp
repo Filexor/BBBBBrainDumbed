26e4aa419aaa88e14ffeff316d4a6c2ef1770270
File_xor <filexor@gmail.com>
2022-04-02 23:51:44 +0900

    update to latest

--- XFVDP1/main.cpp
+++ XFVDP1/main.cpp
@@ -2,10 +2,17 @@
 #include <KHR/khrplatform.h>
 #include <gl/GL.h>
 
+#include <iostream>
+
+using namespace std;
+
 /*#ifndef GL_GLEXT_PROTOTYPES
 #define GL_GLEXT_PROTOTYPES
 #endif // !GL_GLEXT_PROTOTYPES*/
 #include <gl/glext.h>
+
+#include "../BBBBBrainDumbed/BBBBBrainDumbed.h"
+#include "../BBBBBrainDumbed/Parser.h"
 
 void (APIENTRY* glGenBuffers)(GLsizei n, GLuint* buffers);
 void (APIENTRY* glBindBuffer)(GLenum target, GLuint buffer);
@@ -13,6 +20,8 @@ void (APIENTRY* glBufferData)(GLenum target, GLsizeiptr size, const void* data, 
 void (APIENTRY* glEnableVertexAttribArray)(GLuint index);
 void (APIENTRY* glVertexAttribPointer)(GLuint index, GLint size, GLenum type, GLboolean normalized, GLsizei stride, const void* pointer);
 void (APIENTRY* glDisableVertexAttribArray)(GLuint index);
+
+static BBBBBrainDumbed* bbbbbraindumbed = NULL;
 
 static const GLfloat vertData[] = {
     0.0,0.0,
@@ -35,8 +44,6 @@ void disp()
     glDrawArrays(GL_TRIANGLES, 0, 3);
     glDisableVertexAttribArray(0);
 }
-
-HCURSOR hcArrow;
 
 bool initGL()
 {
@@ -73,6 +80,57 @@ bool initGL()
     return true;
 }
 
+int OpenAssembly(LPWSTR file)
+{
+    if (bbbbbraindumbed)
+    {
+        delete bbbbbraindumbed;
+    }
+    wstring exepath, filepath;
+    basic_ifstream<wchar_t> ifs;
+    ifs.open(file);
+    if (ifs.fail())
+    {
+        return 1;
+    }
+    istreambuf_iterator<wchar_t> ifsbegin(ifs), ifsend;
+    wstring finput(ifsbegin, ifsend);
+    ifs.close();
+    list<Token>* tokens = Tokenizer::tokenize(finput, filepath);
+    vector<bool> ROM;
+    Parser parser(tokens, filepath);
+    try
+    {
+        ROM = parser.parse();
+    }
+    catch (const ParserError& e)
+    {
+        wcout << L"Parser error at token:" << e.token.token << L" filename:" << e.token.filename << L" line:" << e.token.line << L" digit:" << e.token.digit << endl << e.what() << endl;
+        return 2;
+    }
+    catch (const runtime_error& e)
+    {
+        wcout << L"Parser error\n" << e.what() << endl;
+        return 3;
+    }
+    bbbbbraindumbed = new BBBBBrainDumbed();
+    bbbbbraindumbed->memory.bakeRom(ROM);
+    LARGE_INTEGER qpc0, qpc1, qpf;
+    QueryPerformanceFrequency(&qpf);
+    QueryPerformanceCounter(&qpc0);
+    for (size_t i = 0; i < 342*262*60*60; i++)
+    {
+        bbbbbraindumbed->P = 0;
+        bbbbbraindumbed->execute(71, false);
+    }
+    QueryPerformanceCounter(&qpc1);
+    wcout << L"P=" << bbbbbraindumbed->P << endl;
+    wcout << (double)(qpc1.QuadPart - qpc0.QuadPart) / qpf.QuadPart << endl;
+    OutputDebugStringW(to_wstring((double)(qpc1.QuadPart - qpc0.QuadPart) / qpf.QuadPart).c_str());
+}
+
+HCURSOR hcArrow;
+
 LRESULT CALLBACK WindowProcedure(_In_ HWND hwnd, _In_ UINT uMsg, _In_ WPARAM wParam, _In_ LPARAM lParam)
 {
     HDC hdc;
@@ -85,6 +143,35 @@ LRESULT CALLBACK WindowProcedure(_In_ HWND hwnd, _In_ UINT uMsg, _In_ WPARAM wPa
     {
     case WM_CLOSE:
         DestroyWindow(hwnd);
+        break;
+    case WM_COMMAND:
+        switch (HIWORD(wParam))
+        {
+        case 0:
+            switch (LOWORD(wParam))
+            {
+            case 1:
+                break;
+            case 2:
+            {
+                wchar_t filename[MAX_PATH];
+                filename[0] = L'\0';
+                OPENFILENAMEW ofn = { sizeof(OPENFILENAMEW), hwnd, NULL, L"All Files\0*.*\0\0", NULL, NULL, NULL, /*lpstrFile*/ filename, MAX_PATH, NULL, NULL, NULL, NULL, /*Flags*/ OFN_ENABLESIZING | OFN_EXPLORER | OFN_FILEMUSTEXIST | OFN_PATHMUSTEXIST, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL };
+                if (GetOpenFileNameW(&ofn))
+                {
+                    OpenAssembly(ofn.lpstrFile);
+                }
+                break;
+            }
+            default:
+                break;
+            }
+            break;
+        case 1:
+            break;
+        default:
+            break;
+        }
         break;
     case WM_DESTROY:
         PostQuitMessage(0);
@@ -158,6 +245,19 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
 	{
 		return -1;
 	}
+
+    HMENU menuMain = CreateMenu();
+    HMENU menuFileParent = CreatePopupMenu();
+
+    AppendMenuW(menuFileParent, MF_GRAYED, 1, L"Open ROM Image");
+    AppendMenuW(menuFileParent, 0, 2, L"Open Assembly File");
+
+    AppendMenuW(menuMain, MF_POPUP, (UINT_PTR)menuFileParent, L"File");
+    if (!SetMenu(hwndMain, menuMain))
+    {
+        return -2;
+    }
+   
     ShowWindow(hwndMain, SW_SHOW);
     UpdateWindow(hwndMain);
 
